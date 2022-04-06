fd9bfef87970a455a4912df45e49f9c8af299041
ChrisStogsdill <chris1stogsdill@gmail.com>
2022-04-06 15:24:18 -0500

    Get text out of wiki page

--- simple-browser-urllib.py
+++ simple-browser-urllib.py
@@ -3,4 +3,4 @@ from bs4 import BeautifulSoup
 
 site = urllib.request.urlopen('https://en.wikipedia.org/wiki/Hygrocybe_miniata')
 soup = BeautifulSoup(site, 'html.parser')
-print(soup.prettify())
+print(soup.get_text())
